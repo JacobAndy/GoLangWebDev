6ff75d40c4288df47e5a2ac8f81e1a4c3d01dca1
Jacob Anderson <Jacob_Anderson@neimanmarcus.com>
2019-03-05 15:43:59 -0600

    starting my lesson of passing functions into templates

---
+++ LanguageReview/templates/passing-func/01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"strings"
+	"text/template"
+)
+
+var tpl *template.Template
+
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+var fm = template.FuncMap{
+	"uc": strings.ToUpper,
+	"ft": firstThree,
+}
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+}
+
+func firstThree(s string) string {
+	s = strings.TrimSpace(s)
+	if len(s) >= 3 {
+		s = s[:3]
+	}
+	return s
+}
+
+func main() {
+
+	jake := sage{
+		Name: "Jacob",
+	}
+
+}
