8002bcc21cc1c5853d44db15b13dd1c76e4e3b34
Jacob Anderson <Jacob_Anderson@neimanmarcus.com>
2019-03-13 09:22:28 -0500

    reading uploaded files, creating mocks and storing on local machine

---
+++ LanguageReview/08-parsing-data/04-form-file/03-enctype/01-default/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+type person struct {
+	FirstName  string
+	LastName   string
+	Subscribed bool
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", i)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+func i(w http.ResponseWriter, req *http.Request) {
+	bs := make([]byte, req.ContentLength)
+	req.Body.Read(bs)
+	body := string(bs)
+	err := tpl.ExecuteTemplate(w, "index.gohtml", body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
