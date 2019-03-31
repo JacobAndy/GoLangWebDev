f5f8b1a90b3ac0dab22a50453664b5e1b6b9cd8d
Jacob Anderson <odistiinct@gmail.com>
2019-03-31 11:28:55 -0500

    set up flow for logging users in

--- LanguageReview/Sessions/02/main.go
+++ LanguageReview/Sessions/02/main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
-	"text/template"
 
 	uuid "github.com/satori/go.uuid"
 )
 
-var tpl *template.Template
-
 type user struct {
 	UserName string
-	fName    string
-	lName    string
+	First    string
+	Last     string
 }
 
-var dbUsers = map[string]user{}      //user id , user
-var dbSessions = map[string]string{} // session id, user id
+var tpl *template.Template
+var dbUsers = map[string]user{}      // user ID, user
+var dbSessions = map[string]string{} // session ID, user ID
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
@@ -28,37 +27,48 @@ func main() {
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
+
 func index(w http.ResponseWriter, req *http.Request) {
-	c, e := req.Cookie("session")
-	if e != nil {
-		v, _ := uuid.NewV4()
-		c := &http.Cookie{
+
+	// get cookie
+	c, err := req.Cookie("session")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
 			Name:  "session",
-			Value: v.String(),
+			Value: sID.String(),
 		}
 		http.SetCookie(w, c)
 	}
+
+	// if the user exists already, get user
 	var u user
 	if un, ok := dbSessions[c.Value]; ok {
 		u = dbUsers[un]
 	}
+
+	// process form submission
 	if req.Method == http.MethodPost {
 		un := req.FormValue("username")
 		f := req.FormValue("firstname")
 		l := req.FormValue("lastname")
-		u := user{un, f, l}
+		u = user{un, f, l}
 		dbSessions[c.Value] = un
 		dbUsers[un] = u
 	}
+
 	tpl.ExecuteTemplate(w, "index.gohtml", u)
 }
+
 func bar(w http.ResponseWriter, req *http.Request) {
-	c, e := req.Cookie("session")
-	if e != nil {
+
+	// get cookie
+	c, err := req.Cookie("session")
+	if err != nil {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
 	}
-	un, ok := dbUsers[c.value]
+	un, ok := dbSessions[c.Value]
 	if !ok {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
---
+++ LanguageReview/Sessions/03-signup/session.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+func getUser(w http.ResponseWriter, req *http.Request) user {
+	c, e := req.Cookie("session")
+	if e != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	var u user
+	if un, ok := dbSessions[c.Value]; ok {
+		u = dbUsers[un]
+	}
+	return u
+}
+
+func alreadyLoggedIn(req *http.Request) bool {
+	c, e := req.Cookie("session")
+	if e != nil {
+		return false
+	}
+	un := dbSessions[c.Value]
+	_, ok := dbUsers[un]
+	return ok
+}
