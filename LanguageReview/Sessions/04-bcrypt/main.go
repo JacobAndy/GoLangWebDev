ee5ccf67e69929671934e18da03f283380f6440b
Jacob Anderson <odistiinct@gmail.com>
2019-03-31 15:57:39 -0500

    encrypting users passwords

--- LanguageReview/Sessions/04-bcrypt/main.go
+++ LanguageReview/Sessions/04-bcrypt/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
+
+	"golang.org/x/crypto/bcrypt"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 var tpl *template.Template
 
 type user struct {
 	UserName string
-	Password string
+	Password []byte
 	First    string
 	Last     string
 }
@@ -41,15 +46,42 @@ func bar(w http.ResponseWriter, req *http.Request) {
 	tpl.ExecuteTemplate(w, "bar.gohtml", u)
 }
 func signup(w http.ResponseWriter, req *http.Request) {
-	if !alreadyLoggedIn(req) {
-
+	if alreadyLoggedIn(req) {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
 	}
 	if req.Method == http.MethodPost {
 		un := req.FormValue("username")
 		p := req.FormValue("password")
 		f := req.FormValue("firstname")
 		l := req.FormValue("lastname")
+		fmt.Println("here", un, p, f, l)
+		if _, ok := dbUsers[un]; ok {
+			http.Error(w, "User name is already taken", http.StatusForbidden)
+			return
+		}
+		sID, _ := uuid.NewV4()
+		c := &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+		dbSessions[c.Value] = un
+		bs, err := bcrypt.GenerateFromPassword([]byte(p), bcrypt.MinCost)
+		if err != nil {
+			http.Error(w, "internal server error", http.StatusInternalServerError)
+			return
+		}
+		u := user{
+			UserName: un,
+			Password: bs,
+			First:    f,
+			Last:     l,
+		}
+		fmt.Println(un)
+		dbUsers[un] = u
 		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
 	}
 	tpl.ExecuteTemplate(w, "signup.gohtml", nil)
 }
